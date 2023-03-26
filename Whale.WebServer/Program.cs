b939a4bd83484cfa265790c81a63dc8b05e92902
Prithvi Prakashkumar Patel <p.patel@engelmann.de>
2023-03-26 22:19:31 +0200

    Whale.Client project added

--- Whale.WebServer/Program.cs
+++ Whale.WebServer/Program.cs
@@ -18,6 +18,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 
@@ -25,7 +29,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Enable SignalR connections
 app.MapBlazorHub();
+
 app.MapFallbackToPage("/_Host");
 
 app.Run();
